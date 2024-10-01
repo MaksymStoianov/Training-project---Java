2731e18d4c3d0bf03f93408d00a70e4694886645
Maksym Stoyanov <sto.maks+github.com@gmail.com>
2024-10-01 08:40:06 +0200

    add homework_15

---
+++ src/homework_15/Computer.java
@@ -0,0 +1,23 @@
+package homework_15;
+
+public class Computer extends Device {
+  // Размер оперативной памяти (в ГБ)
+  private int ramSize;
+
+  // Размер оперативной памяти (в ГБ)
+  private int ssdSize;
+
+  public Computer(String brand, String model, int ramSize) {
+    super(brand, model, false);
+
+    this.ramSize = ramSize;
+  }
+
+  public Computer(String brand, String model, int ramSize, int ssdSize) {
+    super(brand, model, false);
+
+    this.ramSize = ramSize;
+    this.ssdSize = ssdSize;
+  }
+
+}
