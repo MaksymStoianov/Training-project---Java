cb128200c00e7e108feaf8c427203fcc41a3704a
Maksym Stoyanov <sto.maks+github.com@gmail.com>
2024-10-23 16:09:51 +0200

    add homework_18

---
+++ src/homework_18/Calculator.java
@@ -0,0 +1,115 @@
+package homework_18;
+
+/**
+ * Calculator
+ *
+ * @author <a href="stoianov.maksym@gmail.com">Maksym Stoianov</a>
+ */
+public class Calculator {
+
+  /**
+   * Выполняет сложение двух целых чисел (`a + b`).
+   *
+   * @param a Первое число.
+   * @param b Второе число.
+   * @return Результат сложения чисел `a` и `b`.
+   */
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+
+  /**
+   * Выполняет сложение двух дробных чисел (`a + b`).
+   *
+   * @param a Первое число.
+   * @param b Второе число.
+   * @return Результат сложения чисел `a` и `b`.
+   */
+  public static double add(double a, double b) {
+    return a + b;
+  }
+
+
+  /**
+   * Выполняет вычитание целых чисел (`a - b`).
+   *
+   * @param a Первое число.
+   * @param b Второе число.
+   * @return Результат вычитания числа `b` из числа `a`.
+   */
+  public static int subtract(int a, int b) {
+    return a - b;
+  }
+
+
+  /**
+   * Выполняет вычитание дробных чисел (`a - b`).
+   *
+   * @param a Первое число.
+   * @param b Второе число.
+   * @return Результат вычитания числа `b` из числа `a`.
+   */
+  public static double subtract(double a, double b) {
+    return a - b;
+  }
+
+
+  /**
+   * Выполняет умножение двух целых чисел (`a * b`).
+   *
+   * @param a Первое число.
+   * @param b Второе число.
+   * @return Результат умножения чисел `a` и `b`.
+   */
+  public static int multiply(int a, int b) {
+    return a * b;
+  }
+
+
+  /**
+   * Выполняет умножение двух дробных чисел (`a * b`).
+   *
+   * @param a Первое число.
+   * @param b Второе число.
+   * @return Результат умножения чисел `a` и `b`.
+   */
+  public static double multiply(double a, double b) {
+    return a * b;
+  }
+
+
+  /**
+   * Выполняет деление двух целых чисел (`a / b`).
+   *
+   * @param a Делимое (первое число).
+   * @param b Делитель (второе число).
+   * @return Результат деления числа `a` на число `b`.
+   */
+  public static double divide(int a, int b) {
+    if (b == 0) {
+      // Деление на ноль невозможно.
+      return 0;
+    }
+
+    return (double) a / b;
+  }
+
+
+  /**
+   * Выполняет деление двух дробных чисел (`a / b`).
+   *
+   * @param a Делимое (первое число).
+   * @param b Делитель (второе число).
+   * @return Результат деления числа `a` на число `b`.
+   */
+  public static double divide(double a, double b) {
+    if (b == 0) {
+      // Деление на ноль невозможно.
+      return 0;
+    }
+
+    return a / b;
+  }
+
+}
---
+++ src/homework_18/TestDog.java
@@ -0,0 +1,38 @@
+package homework_18;
+
+/**
+ * TestDog
+ *
+ * @author <a href="stoianov.maksym@gmail.com">Maksym Stoianov</a>
+ */
+public class TestDog {
+  public static void main(String[] args) {
+    Dog dog1 = new Dog("Max", 500);
+    dog1.info();
+    int barrierHeight = 750;
+
+    while (true) {
+      int result = dog1.jumpBarrier(barrierHeight);
+
+      switch (result) {
+        case 0:
+          System.out.println("Высота барьера невалидна!");
+          return;
+
+        case 1:
+          System.out.println("Собака никогда не сможет преодолеть этот барьер, тоска ;(");
+          return;
+
+        case 2:
+          System.out.println("Собаке нужно подкачаться! Треним ...");
+          dog1.train();
+        break;
+
+        case 3:
+          dog1.jump();
+          return;
+      }
+    }
+
+  }
+}
