afbf9370175eb4358622aa8bbdf6c63d456d6da4
Maksym Stoyanov <sto.maks+github.com@gmail.com>
2024-10-23 14:27:04 +0200

    add homework_16

---
+++ src/homework_16/TestMagicArray.java
@@ -0,0 +1,37 @@
+package homework_16;
+
+import java.util.Arrays;
+
+/**
+ * TestMagicArray
+ *
+ * @author <a href="stoianov.maksym@gmail.com">Maksym Stoianov</a>
+ */
+public class TestMagicArray {
+  public static void main(String[] args) {
+
+    MagicArray array = new MagicArray();
+
+    array.add(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    System.out.println(array.toString());
+
+    int index = array.indexOf(5);
+    System.out.println("Индекс элемента: " + index);
+
+    boolean isDeleted1 = array.removeByIndex(3);
+    System.out.println("isDeleted1: " + isDeleted1);
+    System.out.println(array.toString());
+
+    array.add(1555);
+    System.out.println(array.toString());
+
+    boolean isDeleted2 = array.removeByValue(7);
+    System.out.println("isDeleted2: " + isDeleted2);
+    System.out.println(array.toString());
+
+    boolean isDeleted3 = array.removeByValue(10000000);
+    System.out.println("isDeleted3: " + isDeleted3);
+    System.out.println(array.toString());
+
+  }
+}
---
+++ src/lesson_22/animals/Dog.java
@@ -0,0 +1,17 @@
+package lesson_22.animals;
+
+public class Dog extends Animal {
+
+
+
+  @Override
+  public void voice() {
+    // super - обращение к объекту родителя
+    super.voice();
+  }
+
+  @Override
+  public String toString() {
+    return super.toString() + " | Dog. Дополнение реализации родительского метода";
+  }
+}
