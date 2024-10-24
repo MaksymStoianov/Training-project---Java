944ea816285fb2e6ceead507a500c6a57bbf1119
Maksym Stoyanov <sto.maks+github.com@gmail.com>
2024-10-24 08:30:54 +0200

    add homework_21

---
+++ src/homework_21/Task1/Passenger.java
@@ -0,0 +1,35 @@
+package homework_21.Task1;
+
+/**
+ * Passenger
+ *
+ * @author Sergey Bugaenko
+ */
+public class Passenger {
+  private static int counterId;
+
+  private final int id;
+  private String name;
+
+  public Passenger(String name) {
+    this.name = name;
+    this.id = ++counterId;
+  }
+
+  public String toString() {
+    return "Passenger id: " + id + " name: " + name;
+  }
+
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/homework_21/Task2/Component.java
@@ -0,0 +1,47 @@
+package homework_21.Task2;
+
+/**
+ * Component
+ *
+ * @author <a href="stoianov.maksym@gmail.com">Maksym Stoianov</a>
+ */
+public class Component {
+  private String brand;
+  private String model;
+
+
+  public Component(String brand, String model) {
+    this.brand = brand;
+    this.model = model;
+  }
+
+
+  /**
+   * Возвращает бренд компонента.
+   *
+   * @return Бренд.
+   */
+  public String getBrand() {
+    return brand;
+  }
+
+
+  /**
+   * Возвращает модель компонента.
+   *
+   * @return Модель.
+   */
+  public String getModel() {
+    return model;
+  }
+
+
+  /**
+   * Возвращает строковое представление текущего класса.
+   *
+   * @return Данные о {@code Component}.
+   */
+  public String toString() {
+    return String.format("Component:\n- Brand: %s\n- Model: %s", this.brand, this.model);
+  }
+}
