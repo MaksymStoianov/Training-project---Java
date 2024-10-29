a510bb277365208e857eebd19a90e7ef6f8986e4
Maksym Stoyanov <sto.maks+github.com@gmail.com>
2024-10-29 07:27:52 +0100

    add: homework_27

---
+++ src/homework_27/Task1/Test.java
@@ -0,0 +1,23 @@
+package homework_27.Task1;
+
+import homework_27.Person;
+
+/**
+ * Test
+ *
+ * @author <a href="stoianov.maksym@gmail.com">Maksym Stoianov</a>
+ */
+public class Test {
+  public static void main(String[] args) {
+    Person person = new Person("email", "password");
+    System.out.println(person);
+
+    System.out.println(person.setEmail("test@mail.com"));
+    System.out.println(person.setEmail("@test@mail.com"));
+    System.out.println(person.setEmail("test@mail.co.m"));
+    System.out.println(person.setEmail("test@mailcom"));
+    System.out.println(person.setEmail("testmail.com"));
+
+    System.out.println(person.getEmail());
+  }
+}
