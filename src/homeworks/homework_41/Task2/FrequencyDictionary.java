3c6fae75ff620cd1c4e23eaae56ca4f4936cfe1c
Maksym Stoianov <stoianov.maksym@gmail.com>
2024-11-07 12:21:19 +0100

    update: homework_41.Task2

---
+++ src/homeworks/homework_41/Task2/FrequencyDictionary.java
@@ -0,0 +1,9 @@
+package homeworks.homework_41.Task2;
+
+/**
+ * FrequencyDictionary
+ * 
+ * @author <a href="stoianov.maksym@gmail.com">Maksym Stoianov</a>
+ */
+public class FrequencyDictionary {
+}
